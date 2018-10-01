ca39cac6e374a07e8c9703116235a8b9f620984c
Marvin Liu <psychicalcoder@gmail.com>
2018-10-01 11:40:05 +0800

    .emacs.d written in org-babel, and some custom snippets.

---
+++ init.el
@@ -0,0 +1,16 @@
+(require 'org)
+(org-babel-load-file "~/.emacs.d/dotfiles.org")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (company-c-headers helm-company yasnippet-snippets yaml-mode web-mode rainbow-delimiters markdown-mode+ helm google-translate f elpy doom-themes))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
