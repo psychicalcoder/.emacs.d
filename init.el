bf68edd625a3f7f8bdfc06541f3772c44d2d151d
Marvin Liu <psychicalcoder@gmail.com>
2019-07-29 16:56:36 +0800

    added vc-mode-line and improve lsp-mode configurations

--- init.el
+++ init.el
@@ -5,9 +5,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("bc75dfb513af404a26260b3420d1f3e4131df752c19ab2984a7c85def9a2917e" default)))
  '(package-selected-packages
    (quote
-    (helm-lsp multi-term exwm lsp-mode json-mode org-plus-contrib company-c-headers helm-company yasnippet-snippets yaml-mode web-mode rainbow-delimiters markdown-mode+ helm google-translate f elpy doom-themes))))
+    (smart-mode-line-atom-one-dark-theme smart-mode-line helm-lsp multi-term exwm lsp-mode json-mode org-plus-contrib company-c-headers helm-company yasnippet-snippets yaml-mode web-mode rainbow-delimiters markdown-mode+ helm google-translate f elpy doom-themes))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
