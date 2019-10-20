83e860fb0a6bb755ce28eeafe18aab89d16653e5
Marvin Liu <psychicalcoder@gmail.com>
2019-10-20 15:44:28 +0800

    added dried and remove some useless packages.

--- init.el
+++ init.el
@@ -10,7 +10,7 @@
     ("bc75dfb513af404a26260b3420d1f3e4131df752c19ab2984a7c85def9a2917e" default)))
  '(package-selected-packages
    (quote
-    (smart-mode-line-atom-one-dark-theme smart-mode-line helm-lsp multi-term exwm lsp-mode json-mode org-plus-contrib company-c-headers helm-company yasnippet-snippets yaml-mode web-mode rainbow-delimiters markdown-mode+ helm google-translate f elpy doom-themes))))
+    (helm-mt gnu-elpa-keyring-update smart-mode-line-atom-one-dark-theme smart-mode-line helm-lsp multi-term exwm lsp-mode json-mode org-plus-contrib company-c-headers helm-company yasnippet-snippets yaml-mode web-mode rainbow-delimiters markdown-mode+ helm google-translate f elpy doom-themes))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
