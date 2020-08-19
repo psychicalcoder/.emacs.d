5ad23ebf35476185915143e6e40782d39d7c58a8
Marvin Liu <psychicalcoder@gmail.com>
2020-08-19 11:14:27 +0800

    removed ox-reveal added tramp-2.4.4 tested in 27.1

--- init.el
+++ init.el
@@ -6,11 +6,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   (quote
-    ("bc75dfb513af404a26260b3420d1f3e4131df752c19ab2984a7c85def9a2917e" default)))
+   '("bc75dfb513af404a26260b3420d1f3e4131df752c19ab2984a7c85def9a2917e" default))
  '(package-selected-packages
-   (quote
-    (helm-mt gnu-elpa-keyring-update smart-mode-line-atom-one-dark-theme smart-mode-line helm-lsp multi-term exwm lsp-mode json-mode org-plus-contrib company-c-headers helm-company yasnippet-snippets yaml-mode web-mode rainbow-delimiters markdown-mode+ helm google-translate f elpy doom-themes))))
+   '(tramp ox-reveal helm-mt gnu-elpa-keyring-update smart-mode-line-atom-one-dark-theme smart-mode-line helm-lsp multi-term exwm lsp-mode json-mode org-plus-contrib company-c-headers helm-company yasnippet-snippets yaml-mode web-mode rainbow-delimiters markdown-mode+ helm google-translate f elpy doom-themes)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
